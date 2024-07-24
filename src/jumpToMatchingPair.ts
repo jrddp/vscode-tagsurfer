053d675fc041e8763d4c5f5f149cdfc5cb5184b4
Jared Peters <jar3dpt@gmail.com>
2024-07-24 13:31:19 -0400

    scaffold

---
+++ src/jumpToMatchingPair.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+export function jumpToMatchingPair() {
+  // Display a message
+  vscode.window.showInformationMessage("TagSurfer: Jump to Matching Pair command executed!");
+
+  // TODO: Implement the actual jump to matching pair functionality
+  console.log("Jump to Matching Pair functionality will be implemented here.");
+}
---
+++ src/surroundWithTag.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+export function surroundWithTag() {
+  // Display a message
+  vscode.window.showInformationMessage("TagSurfer: Surround with Tag command executed!");
+
+  // TODO: Implement the actual jump to matching pair functionality
+  console.log("Jump to Matching Pair functionality will be implemented here.");
+}
