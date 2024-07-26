c79b17b3ee5a47a4b09b18d8d80b169d32382e90
Jared Peters <jar3dpt@gmail.com>
2024-07-26 01:02:25 -0400

    Scaffold deleteSelectionWithPairs

---
+++ src/deleteSelectionWithPairs.ts
@@ -0,0 +1,6 @@
+import * as vscode from "vscode";
+
+export function deleteSelectionWithMatchingPairs() {
+  // display test message
+  vscode.window.showInformationMessage("Hello World!");
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import { jumpToMatchingPair } from "./jumpToMatchingPair";
 import { surroundWithTag } from "./surroundWithTag";
 import { insertSelfClosingTag } from "./insertSelfClosingTag";
 import { deleteSurroundingTagPair } from "./deleteSurroundingTagPair";
+import { deleteSelectionWithMatchingPairs } from "./deleteSelectionWithPairs";
 
 export function activate(context: vscode.ExtensionContext) {
   let surroundDisposable = vscode.commands.registerCommand(
@@ -17,16 +18,21 @@ export function activate(context: vscode.ExtensionContext) {
     "tagSurfer.insertSelfClosingTag",
     insertSelfClosingTag
   );
-  let deleteDisposable = vscode.commands.registerTextEditorCommand(
+  let deleteSurroundingDisposable = vscode.commands.registerTextEditorCommand(
     "tagSurfer.deleteSurroundingTagPair",
     deleteSurroundingTagPair
+  );
+  let deleteWithPairsDisposable = vscode.commands.registerTextEditorCommand(
+    "tagSurfer.deleteSelectionWithPairs",
+    deleteSelectionWithMatchingPairs
   );
 
   context.subscriptions.push(
     surroundDisposable,
     jumpDisposable,
     selfClosingDisposable,
-    deleteDisposable
+    deleteSurroundingDisposable,
+    deleteWithPairsDisposable
   );
 }
 
